5bbef243df09f4572dc801f65fa099ff64a5ed32
Arc0fLife <ahmedyehia3633@yahoo.com>
2021-10-15 21:46:00 +0300

    Sixth commit

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,7 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-import './styles/resets'
+import './styles/resets.scss'
 
 import { checkUrl } from './js/checkURL'
 import { handleSubmit } from './js/formHandler'
